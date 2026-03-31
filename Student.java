a32a1be02ddeab918c3aeb3ac569e472963842f4
rakshaknidhi <nidhivrakshak@gmail.com>
2026-03-31 22:51:19 +0530

    Creat Student object and display details

---
+++ Student.java
@@ -0,0 +1,19 @@
+class Student {
+    int id;
+    String name;
+
+    Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name;
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student(101, "Nidhi");
+
+        System.out.println(s); // Calls toString()
+    }
+}
